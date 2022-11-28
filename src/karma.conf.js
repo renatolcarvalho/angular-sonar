c93e92265d531ac47e64de89f4e34f523eb09145
Renato Luiz Carvalho <renato.carvalho@programmers.com.br>
2022-11-28 18:37:32 -0300

    WIP

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,98 +1,42 @@
-// Karma configuration
-// Generated on Mon Nov 28 2022 18:01:14 GMT-0300 (Brasilia Standard Time)
-
+// Example of a typical configuration
 module.exports = function (config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-sonarqube-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-sonarqube-reporter')
     ],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      {
-        pattern: 'app/**/*.spec.ts',
-        type: 'js'
-      }
-    ],
-
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {
+    client:{
+      clearContext: false
     },
-
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
     sonarqubeReporter: {
-      basePath: 'src/app',        // test files folder
-      filePattern: '**/*spec.ts', // test files glob pattern
-      encoding: 'utf-8',          // test files encoding
-      outputFolder: 'reports',    // report destination
-      legacyMode: false,          // report for Sonarqube < 6.2 (disabled)
-      reportName: (metadata) => { // report name callback, but accepts also a 
-                                  // string (file name) to generate a single file
-        /**
-         * Report metadata array:
-         * - metadata[0] = browser name
-         * - metadata[1] = browser version
-         * - metadata[2] = plataform name
-         * - metadata[3] = plataform version
-         */
-         return metadata.concat('xml').join('.');
+      basePath: 'src/app',
+      outputFolder: 'reports',
+      filePattern: '**/*spec.ts',
+      encoding: 'utf-8',
+      legacyMode: false,
+      reportName: (metadata) => {
+        return metadata.concat('xml').join('.');
       }
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress', 'sonarqube'],
-
-
-    // web server port
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml', 'sonarqube'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser instances should be started simultaneously
-    concurrency: Infinity
-  })
-}
+    singleRun: false
+  });
+};
